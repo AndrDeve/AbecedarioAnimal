a22931bbf9336a3a82886dd8f95c5deab7236202
AndyR <marlon_rivera95@hotmail.com>
2021-11-26 16:06:31 -0500

    se agrega el proyecto abecedario

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+ScrollReveal().reveal('.rey-nav', { delay: 400 });
+
+ScrollReveal().reveal('.titulos-animales', { delay: 400 });
+
+window.sr = ScrollReveal();
+
+    sr.reveal('.selecciona', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '50px'
+    });
+
+
+    window.sr = ScrollReveal();
+
+    sr.reveal('.rey-ima', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '50px'
+    });
+
+
+window.sr = ScrollReveal();
+
+    sr.reveal('.tarjeta', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '50px'
+    });    
